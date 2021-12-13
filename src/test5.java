f4e4253e0046d6f504cffd2f1aba9f5062287286
Ankur <ankurgupta032002@gmail.com>
2021-12-13 21:56:11 +0530

    Project Added

--- src/test5.java
+++ src/test5.java
@@ -6,13 +6,15 @@
 
         If N is 101, the tens digit is 0.
         */
-import java.util.*;
-class code1 {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println(n %100 / 10);
-    }
-}
+//import java.util.*;
+//class code1 {
+//    public static void main(String args[])
+//    {
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        System.out.println(n %100 / 10);
+//    }
+//}
+//
+
 
